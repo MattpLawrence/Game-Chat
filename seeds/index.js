f14e9f6f7c3c8553c6f9f671b2645268dca3e00f
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-02 18:50:02 -0500

    add seed file, filling out seed info for gameslist

--- seeds/index.js
+++ seeds/index.js
@@ -1,3 +1,23 @@
 // seed the database with initial/ test data
+
+//connect to sequelize
 const sequelize = require("../config/connection");
+//require tables
 const { UserProfile, UserGames, GamesList } = require("../models/index");
+
+//require in .json seed files
+const seedGamesList = require("./seedGamesList.json");
+const seedUserGames = require("./seedUserGames.json");
+const seedUserProfile = require("./seedUserProfile.json");
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const userProfile = await UserProfile.bulkCreate(seedUserProfile);
+  const userGames = await UserGames.bulkCreate(seedUserGames);
+  const gamesList = await GamesList.bulkCreate(seedGamesList);
+
+  process.exit(0);
+};
+
+seedDatabase();
