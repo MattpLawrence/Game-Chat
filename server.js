7728ab32ff0e47eb8d893e637b4b5bccab98d299
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-02 16:37:18 -0500

    update server.js to include sequelize

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const sequelize = require("./config/connection");
+const path = require("path");
 
 const app = express();
 
 const PORT = process.envPORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port: ${PORT}`);
+app.use(express.json);
+app.use(express.urlencoded({ extended: false }));
+// set root of static assets tot he 'public' folder
+app.use(express.static(path.join(__dirname, "public")));
+
+//make sure sequelize is connected before starting the server.
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+  });
 });
