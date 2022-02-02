067b78585328f87b61e1dac09bf0c314cff2244e
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-02 16:52:42 -0500

    adding class to userProfile.js

---
+++ models/userProfile.js
@@ -0,0 +1,44 @@
+//require Model and DataTypes from Sequelize package
+const { Model, DataTypes } = require("sequelize");
+//connect to the sequelize server created in config/connection
+const sequelize = require("../config/connection");
+
+class UserProfile extends Model {}
+
+UserProfile.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  nameUser: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  passUser: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  nameDisplay: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  ageUser: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  timeStart: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  timeEnd: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+});
