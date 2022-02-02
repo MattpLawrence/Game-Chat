675898317a57504ced45ebebda080e651364481f
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-02 17:04:37 -0500

    update listGames.js for tblListGames

---
+++ models/listGames.js
@@ -0,0 +1,33 @@
+//require Model and DataTypes from Sequelize package
+const { Model, DataTypes } = require("sequelize");
+//connect to the sequelize server created in config/connection
+const sequelize = require("../config/connection");
+
+class ListGames extends Model {}
+
+ListGames.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    namePlatform: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    nameGame: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "tblListGames",
+  }
+);
+
+module.exports = ListGames;
