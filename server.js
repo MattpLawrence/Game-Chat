e439655d355f6c7f8a8c0535d2c29c2ce64c031a
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-02 16:24:56 -0500

    add config folder and connections for connecting sql to server

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-let express = require("express");
+const express = require("express");
+
+const app = express();
+
+const PORT = process.envPORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}`);
+});
