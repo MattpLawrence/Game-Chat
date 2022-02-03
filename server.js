8311b6e3792535868b52533308915f0b36f6719b
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-03 13:40:05 -0500

    working on server setup for pusher

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 const express = require("express");
-const sequelize = require("./config/connection");
 const path = require("path");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const Pusher = require("pusher");
+
 //require in models folder in order to generate the tables using sequelize
 const models = require("./models");
 const routes = require("./controllers");
+const sequelize = require("./config/connection");
+require("dotenv").config();
 
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 
+// Session middleware
+app.use(
+  session({
+    secret: "somesuperdupersecret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 // set root of static assets tot he 'public' folder
 app.use(express.static(path.join(__dirname, "public")));
 
