c8f5201d4652ee83020e9db8f60df0608e575c3a
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-03 12:28:28 -0500

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -3,9 +3,11 @@ const { GamesList, UserGames, UserProfile } = require("../models");
 
 router.get("/", async (req, res) => {
   try {
-    const dataUserProfile = await UserProfile.findAll();
-    console.log("got");
-    return res.status(200).json(dataUserProfile);
+    // const dataUserProfile = await UserProfile.findAll();
+    // const dataUserGames = await UserGames.findAll();
+    const dataGamesList = await GamesList.findAll();
+    console.log("got it");
+    return res.status(200).json(dataGamesList);
   } catch (err) {
     console.log("error");
     return res.status(500).json(err);
