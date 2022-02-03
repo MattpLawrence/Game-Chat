4dbe512961e4941675f1a6acdb4845e78f8e9487
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-02 22:52:58 -0500

    add seedUserGames.json content

--- seeds/index.js
+++ seeds/index.js
@@ -7,14 +7,14 @@ const { UserProfile, UserGames, GamesList } = require("../models/index");
 
 //require in .json seed files
 
-// const seedUserProfile = require("./seedUserProfile.json");
+const seedUserProfile = require("./seedUserProfile.json");
 // const seedUserGames = require("./seedUserGames.json");
 const seedGamesList = require("./seedGamesList.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  // const userProfile = await UserProfile.bulkCreate(seedUserProfile);
+  const userProfile = await UserProfile.bulkCreate(seedUserProfile);
   // await UserGames.bulkCreate(seedUserGames);
   const gamesList = await GamesList.bulkCreate(seedGamesList);
 
