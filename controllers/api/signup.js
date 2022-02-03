da4586c97b7d16945e78dd80c39d67a17da4313d
Lanay Brown <lanayb.03@gmail.com>
2022-02-03 12:51:27 -0500

    saving my changes

--- controllers/api/signup.js
+++ controllers/api/signup.js
@@ -5,6 +5,9 @@ const { UserProfile } = require("../../models");
 //take info from body, save to dbGameChat
 router.post("/signupProfile", async (req, res) => {
   try {
+    res.status(200).send({
+      message: 'working'
+    });
   } catch (err) {
     res.status(400).json(err);
   }
