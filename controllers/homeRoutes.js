bbc804a24058a93f0644aa6798be911244f6aef5
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-03 11:05:15 -0500

    update homeroutes to include basic usergames route. add api/addGames route framework

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -26,3 +26,12 @@ router.get("/", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+//get and display the add games page, make sure to include query to show the user's already added games once a platform has been selected.
+router.get("/addGames:id", async (req, res) => {
+  try {
+    res.render("addGames", {});
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
