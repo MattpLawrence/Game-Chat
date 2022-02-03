f90fbce2bb7a33fa8de23ea097865caaf54bd877
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-03 14:42:46 -0500

    Create app.js

---
+++ public/app.js
@@ -0,0 +1,54 @@
+// public/app.js
+
+const pusher = new Pusher("xxxxxxxxxxxxxxxxxxxx", {
+  cluster: "APP_CLUSTER",
+  encrypted: true,
+  authEndpoint: "pusher/auth",
+});
+const app = new Vue({
+  el: "#app",
+  data: {
+    joined: false,
+    username: "",
+    members: "",
+    newMessage: "",
+    messages: [],
+    status: "",
+  },
+  methods: {
+    joinChat() {
+      axios.post("join-chat", { username: this.username }).then((response) => {
+        // User has joined the chat
+        this.joined = true;
+        const channel = pusher.subscribe("presence-groupChat");
+        channel.bind("pusher:subscription_succeeded", (members) => {
+          this.members = channel.members;
+        });
+        // User joins chat
+        channel.bind("pusher:member_added", (member) => {
+          this.status = `${member.id} joined the chat`;
+        });
+        // Listen for chat messages
+        this.listen();
+      });
+    },
+    sendMessage() {
+      let message = {
+        username: this.username,
+        message: this.newMessage,
+      };
+      // Clear input field
+      this.newMessage = "";
+      axios.post("/send-message", message);
+    },
+    listen() {
+      const channel = pusher.subscribe("presence-groupChat");
+      channel.bind("message_sent", (data) => {
+        this.messages.push({
+          username: data.username,
+          message: data.message,
+        });
+      });
+    },
+  },
+});
