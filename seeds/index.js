1234fe31f9c010e33513512c4ac5ba7797f0fc5d
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-02 23:01:44 -0500

    change seeds file to match sequelise snake case

--- seeds/index.js
+++ seeds/index.js
@@ -8,15 +8,15 @@ const { UserProfile, UserGames, GamesList } = require("../models/index");
 //require in .json seed files
 
 const seedUserProfile = require("./seedUserProfile.json");
-// const seedUserGames = require("./seedUserGames.json");
+const seedUserGames = require("./seedUserGames.json");
 const seedGamesList = require("./seedGamesList.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const userProfile = await UserProfile.bulkCreate(seedUserProfile);
-  // await UserGames.bulkCreate(seedUserGames);
-  const gamesList = await GamesList.bulkCreate(seedGamesList);
+  await UserProfile.bulkCreate(seedUserProfile);
+  await UserGames.bulkCreate(seedUserGames);
+  await GamesList.bulkCreate(seedGamesList);
 
   process.exit(0);
 };
