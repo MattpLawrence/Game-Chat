d44b428a1eafdd4ef133268e61aa0dbcf1eeb175
Lanay Brown <lanayb.03@gmail.com>
2022-02-04 10:01:59 -0500

    saving my changes

--- controllers/api/signup.js
+++ controllers/api/signup.js
@@ -3,12 +3,11 @@ const res = require("express/lib/response");
 const { UserProfile } = require("../../models");
 
 //take info from body, save to dbGameChat
-router.post("/signupProfile", async (req, res) => {
-  try {
-    res.status(200).send({
-      message: 'working'
-    });
-  } catch (err) {
+router.post('/signupProfile', async(req, res) => {
+  try{
+    const userProfileData = await UserProfile.create(req.body);
+    res.status(200).json(userProfileData);
+  } catch(err) {
     res.status(400).json(err);
   }
 });
@@ -18,3 +17,5 @@ router.post("/signupProfile", async (req, res) => {
 // reference api routes on loginPage.js to log in after creating account
 
 module.exports = router;
+
+
