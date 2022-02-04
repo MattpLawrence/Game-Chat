9ae49eaa2070d5741f34ffa7c9f0d492c74cd054
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-04 14:36:33 -0500

    update server.js to include chat info

--- server.js
+++ server.js
@@ -9,8 +9,17 @@ const socketio = require("socket.io");
 //require in models folder in order to generate the tables using sequelize
 const models = require("./models");
 const routes = require("./controllers");
+const formatMessage = require("./utils/messages");
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const PORT = process.env.PORT || 3001;
 
@@ -24,6 +33,63 @@ app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
 app.use(routes);
+
+// ************************************Socket.io**************************************
+const botName = "Gamer Gabble Bot";
+
+// run when client connects
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
+
+    socket.join(user.room);
+
+    //emit object to be grabbed with socket.on() in main.js to be viewable only the one connecting
+    socket.emit("message", formatMessage(botName, "Welcome to Gamer Gabble"));
+
+    // Broadcast when user connects to everyone but user in the room
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
+
+    // SEND USER AND ROOM INFO
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+  });
+
+  // listen for ChatMessage to be submitted
+  socket.on("chatMessage", (msg) => {
+    const user = getCurrentUser(socket.id);
+
+    // submit the message to everyone
+    io.to(user.room).emit("message", formatMessage(user.username, msg));
+  });
+
+  // Run when client disconnects
+  socket.on("disconnect", () => {
+    const user = userLeave(socket.id);
+
+    if (user) {
+      // broadcast to everyone
+      io.to(user.room).emit(
+        "message",
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+      // send user room info
+      io.to(user.room).emit("roomUsers", {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      });
+    }
+  });
+});
+
+// ************************************Socket.io**************************************
 
 //make sure sequelize is connected before starting the server.
 sequelize.sync({ force: false }).then(() => {
