02a6fd486f98aae29a1f8208bc8f8dbfa33ecf19
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-06 17:44:53 -0500

    Update signUpProfile.js

--- public/js/signUpProfile.js
+++ public/js/signUpProfile.js
@@ -9,6 +9,41 @@ console.log("yup");
 
 btnRegister.addEventListener("click", checkInputs);
 
+let postUser = async () => {
+  const user = nameUser.value.trim();
+  const display = nameDisplay.value.trim();
+  const pass = password.value.trim();
+
+  if (user && display && pass) {
+    const response = await post(`/api/signup/signupProfile`, {
+      method: "POST",
+      body: JSON.stringify({
+        name_user,
+        pass_user,
+        name_display,
+        age_user,
+        time_start,
+        time_end,
+      }),
+      headers: {
+        name_user: user,
+        pass_user: pass,
+        name_display: display,
+        age_user: "1",
+        time_start: "06:00AM",
+        time_end: "12:00PM",
+      },
+    });
+
+    if (response.ok) {
+      console.log("success");
+      // document.location.replace("/profile");
+    } else {
+      alert("Failed to create project");
+    }
+  }
+};
+
 function checkInputs() {
   let alert = document.createElement("h3");
   alert.classList.add("alert");
@@ -56,68 +91,6 @@ function checkInputs() {
     }
   } else {
     console.log("complete");
-    postUser()
-  //   const response = await post(`/api/signup/signupProfile`, {
-  //     method: "POST",
-  //     body: JSON.stringify({
-  //       name_user,
-  //       pass_user,
-  //       name_display,
-  //       age_user,
-  //       time_start,
-  //       time_end,
-  //     }),
-  //     headers: {
-  //       name_user: nameUser.value,
-  //       pass_user: password.value,
-  //       name_display: nameDisplay.value,
-  //       age_user: "1",
-  //       time_start: "06:00AM",
-  //       time_end: "12:00PM",
-  //     },
-  //   });
-
-  //   if (response.ok) {
-  //     console.log("success");
-  //     // document.location.replace("/profile");
-  //   } else {
-  //     alert("Failed to create project");
-  //   }
-  // }
-}
-
-let postUser = async() => {
-
-  const user = document.querySelector('#project-name').value.trim();
-  const display = document.querySelector('#project-funding').value.trim();
-  const pass = document.querySelector('#project-desc').value.trim();
-
-  if(user && display && pass)
-    const response = await post(`/api/signup/signupProfile`, {
-      method: "POST",
-      body: JSON.stringify({
-        name_user,
-        pass_user,
-        name_display,
-        age_user,
-        time_start,
-        time_end,
-      }),
-      headers: {
-        name_user: user,
-        pass_user: pass,
-        name_display: display,
-        age_user: "1",
-        time_start: "06:00AM",
-        time_end: "12:00PM",
-      },
-    });
-
-    if (response.ok) {
-      console.log("success");
-      // document.location.replace("/profile");
-    } else {
-      alert("Failed to create project");
-    };
+    postUser();
   }
 }
