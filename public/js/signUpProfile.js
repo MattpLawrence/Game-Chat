dfa8bb54cab6657d7a14f500f45f352e20115146
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-06 16:13:21 -0500

    getting on eter to work

--- public/js/signUpProfile.js
+++ public/js/signUpProfile.js
@@ -1,12 +1,21 @@
 const nameUser = document.getElementById("nameUser");
-const nameDisplay = document.getElementById("nameUser");
-const password = document.getElementById("nameUser");
-const confirmPassword = document.getElementById("nameUser");
+const nameDisplay = document.getElementById("nameDisplay");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
 const btnRegister = document.getElementById("btnRegister");
+const frmRegister = document.getElementById("frmRegister");
+console.log("yup");
 
-function register() {
-  btnRegister.addEventListener("click", checkInputs);
-}
+frmRegister.addEventListener("submit", checkInputs);
+
 function checkInputs() {
-  console.log(nameUser);
+  console.log(
+    nameUser.value + nameDisplay.value + password.value + confirmPassword.value
+  );
+}
+
+function checkSubmit(e) {
+  if (e && e.keyCode == 13) {
+    nameUser.value + nameDisplay.value + password.value + confirmPassword.value;
+  }
 }
