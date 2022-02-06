f7da376de54b2c886fd84ff381b614a8553fc9e2
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-06 17:07:12 -0500

    Update signUpProfile.js

--- public/js/signUpProfile.js
+++ public/js/signUpProfile.js
@@ -10,35 +10,19 @@ console.log("yup");
 btnRegister.addEventListener("click", checkInputs);
 
 function checkInputs() {
-  console.log(
-    nameUser.value + nameDisplay.value + password.value + confirmPassword.value
-  );
   let alert = document.createElement("h3");
   alert.classList.add("alert");
 
-  if (nameUser.value.length >= 6) {
-    console.log(nameUser.value.length);
+  if (!(nameUser.value.length >= 6)) {
+    if (frmRegister.children.length < 6) {
+      console.log(frmRegister.children.length);
+      alert.innerHTML = `*Please enter a username with at least 6 characters.`;
+      // alert.text("Please enter").css("color", "red").addClass("error-txt");
+      frmRegister.append(alert);
+    } else {
+      frmRegister.firstElementChild.remove;
+      console.log("else");
+    }
   } else {
-    alert.innerHTML = `*Please enter a username with at least 6 characters.`;
-    // alert.text("Please enter").css("color", "red").addClass("error-txt");
-    frmRegister.append(alert);
-    console.log("else");
   }
 }
-
-// function trim(str) {
-//   return str.trim;
-// }
-
-// function checkInputs() {
-//   console.log(
-//     nameUser.value + nameDisplay.value + password.value + confirmPassword.value
-//   );
-
-//   if (nameUser.value.length >= 6) {
-//     console.log(nameUser.value.length);
-//   } else {
-//     console.log("else");
-//     nameUser.value = "please";
-//   }
-// }
