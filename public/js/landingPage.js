99eadfd4c621b19f6e4701641c1518c3066fe675
Lanay Brown <lanayb.03@gmail.com>
2022-02-06 13:28:37 -0500

    updating

--- public/js/landingPage.js
+++ public/js/landingPage.js
@@ -1,3 +1,14 @@
+const btnSignUp = document.getElementById('btn');
+const BtnLogin = document.getElementById('BtnLogin');
+
+btnSignUp.onclick = () => {
+    
+    console.log('working!');
+};
+
+BtnLogin.onclick = () => {
+    console.log('click!');
+};
 
 
 
