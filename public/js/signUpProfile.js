e9dc1effba4153b655405fd677168cbc97ebe01f
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-06 16:52:19 -0500

    omg...finally got the register warning working

--- public/js/signUpProfile.js
+++ public/js/signUpProfile.js
@@ -4,18 +4,41 @@ const password = document.getElementById("password");
 const confirmPassword = document.getElementById("confirmPassword");
 const btnRegister = document.getElementById("btnRegister");
 const frmRegister = document.getElementById("frmRegister");
+
 console.log("yup");
 
-frmRegister.addEventListener("submit", checkInputs);
+btnRegister.addEventListener("click", checkInputs);
 
 function checkInputs() {
   console.log(
     nameUser.value + nameDisplay.value + password.value + confirmPassword.value
   );
-}
+  let alert = document.createElement("h3");
+  alert.classList.add("alert");
 
-function checkSubmit(e) {
-  if (e && e.keyCode == 13) {
-    nameUser.value + nameDisplay.value + password.value + confirmPassword.value;
+  if (nameUser.value.length >= 6) {
+    console.log(nameUser.value.length);
+  } else {
+    alert.innerHTML = `*Please enter a username with at least 6 characters.`;
+    // alert.text("Please enter").css("color", "red").addClass("error-txt");
+    frmRegister.append(alert);
+    console.log("else");
   }
 }
+
+// function trim(str) {
+//   return str.trim;
+// }
+
+// function checkInputs() {
+//   console.log(
+//     nameUser.value + nameDisplay.value + password.value + confirmPassword.value
+//   );
+
+//   if (nameUser.value.length >= 6) {
+//     console.log(nameUser.value.length);
+//   } else {
+//     console.log("else");
+//     nameUser.value = "please";
+//   }
+// }
