33f81ee2db26723907b7d7ec0a21be73349fa3d7
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-06 21:28:08 -0500

    login route basically set without auth

--- controllers/api/login.js
+++ controllers/api/login.js
@@ -5,56 +5,22 @@ const { UserProfile } = require("../../models");
 router.post("/", async (req, res) => {
   console.log(req.body);
   try {
-    const userData = await UserProfile.findOne({
-      where: { name_user: req.body.username },
-    });
-    console.log(userData);
-
-    if (!userData) {
-      res
-        .status(400)
-        .json({ message: "Incorrect email or password, please try again" });
-      return;
-    }
-
-    const validPassword = await userData.checkPassword(req.body.pass_user);
-
-    if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: "Incorrect email or password, please try again" });
-      return;
-    }
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-
-      res.json({ user: userData, message: "You are now logged in!" });
-    });
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
-router.post("/login", async (req, res) => {
-  console.log(req.body);
-  try {
     const userProfileData = await UserProfile.findOne({
       where: { name_user: req.body.name_user },
     });
+    let password = userProfileData.pass_user;
     if (!userProfileData) {
       res
         .status(200)
         .json({ message: "Incorrect email or password, please try again" });
+    } else if (req.body.pass_user !== password) {
+      res
+        .status(200)
+        .json({ message: "Incorrect email or password, please try again" });
+    } else {
+      console.log(userProfileData);
+      res.status(200).json(`${userProfileData.name_display} has logged in.`);
     }
-
-    const validPassword = await userProfileData.checkPassword(
-      req.body.pass_user
-    );
-
-    console.log(userProfileData);
-    res.status(200).json(userProfileData);
   } catch (err) {
     console.log(err);
     res.status(400).json(err);
