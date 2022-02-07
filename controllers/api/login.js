9ad5c68c72be024d5612e013faa020be360f3f10
Matthew Lawrence <Mattplawrence@gmail.com>
2022-02-07 10:21:09 -0500

    update dependencies

--- controllers/api/login.js
+++ controllers/api/login.js
@@ -19,12 +19,12 @@ router.post("/", async (req, res) => {
       console.log(userProfileData);
       // set up session on login success
       console.log(req.session);
-      // req.session.save(() => {
-      //   req.session.user_id = userProfileData.id;
-      //   req.session.logged_in = true;
-
-      res.status(200).json(`${userProfileData.name_display} has logged in.`);
-      // });
+      req.session.save(() => {
+        req.session.user_id = userProfileData.id;
+        req.session.logged_in = true;
+        console.log(req.session.logged_in);
+        res.status(200).json(`${userProfileData.name_display} has logged in.`);
+      });
     }
   } catch (err) {
     console.log(err);
